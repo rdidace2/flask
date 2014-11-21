faa0c62470f5ae31d0063061c30852a2e6bf4bef
rdidace2 <gaceyeyekarambi@yahoo.com>
2014-11-21 09:04:16 +0200

    new commit

--- web.py
+++ web.py
@@ -3,17 +3,24 @@ from flask import Flask
 from flask import request, make_response
 from flask import  redirect
 
-#from web import app
-# from flask import current_app
-
 
 app = Flask(__name__)
 
-# current_app.name
-#
-# app_ctx = app.app_context()
-# app_ctx.push()
-# current_app.name
+@app.route('/user/<name>')
+
+def user(name):
+
+    return '<h1>Hello,%s!</h1>' %name
+
+
+@app.route('/')
+
+def index2():
+
+    response = make_response('<h3> Hello World!</h3>')
+
+    return response
+
 
 @app.route('/facebook')
 
@@ -48,15 +55,15 @@ def goto_igihe():
     return response
 
 
-@app.route('/')
-
-def index1():
-
-    user_agent =request.headers.get('User_Agent')
-
-    response = make_response ( "<h2> The browser is %s</h2>" %user_agent)
-
-    return response
+# @app.route('/')
+#
+# def index1():
+#
+#     user_agent =request.headers.get('User_Agent')
+#
+#     response = make_response ( "<h2> The browser is %s</h2>" %user_agent)
+#
+#     return response
 
 @app.route('/kigali')
 
@@ -122,4 +129,5 @@ def counter3(value):
 
 
 if __name__=='__main__':
+    #server start up
     app.run(debug=True)
